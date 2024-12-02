5d2a097da1663e4b66b6f0573b6119113f40a130
daswi <daswi@DANIEL>
2024-12-01 23:27:59 -0500

    90% funcional

--- PetFinderMVC1/Controllers/HomeController.cs
+++ PetFinderMVC1/Controllers/HomeController.cs
@@ -1,10 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using PetFinderMVC1.Models;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
 
-namespace TuProyecto.Controllers
+namespace PetFinderMVC1.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly HttpClient _httpClient;
+
+        public HomeController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
         public IActionResult Index()
         {
             var userName = Request.Cookies["UserName"];
@@ -13,7 +26,7 @@ namespace TuProyecto.Controllers
             if (string.IsNullOrEmpty(userName) && User.Identity.IsAuthenticated)
             {
                 userName = User.Identity.Name;
-                userLastName = "Apellido"; 
+                userLastName = "Apellido";
             }
 
             ViewBag.UserName = userName;
@@ -22,6 +35,112 @@ namespace TuProyecto.Controllers
             return View("~/Views/Home/Index.cshtml");
         }
 
+        // Método para obtener los detalles de una publicación por ID
+        public async Task<IActionResult> DetallesAdopcion(string id)
+        {
+            var queryPublicaciones = @"
+    query {
+        publicaciones {
+            id
+            foto
+            descripcion
+            nombre
+            especie
+            raza
+            color
+            tamanho
+            sexo
+            ubicacionId
+        }
+    }";
+
+            var requestBody = new StringContent(
+                JsonConvert.SerializeObject(new { query = queryPublicaciones }),
+                Encoding.UTF8,
+                "application/json"
+            );
+
+            var response = await _httpClient.PostAsync("http://petfinder.somee.com/graphql/", requestBody);
+
+            if (!response.IsSuccessStatusCode)
+                return View("Error");
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var jsonResponse = JsonConvert.DeserializeObject<dynamic>(responseContent);
+
+            var publicaciones = jsonResponse.data.publicaciones.ToObject<List<Publicacion>>();
+
+            Publicacion publicacion = null;
+            foreach (var p in publicaciones)
+            {
+                if (p.Id == id)
+                {
+                    publicacion = p;
+                    break;
+                }
+            }
+
+            if (publicacion == null)
+                return NotFound();
+
+            return View(publicacion);
+        }
+        public async Task<IActionResult> DetallesPerdida(string id)
+        {
+            // Query GraphQL para obtener las publicaciones
+            var queryPublicaciones = @"
+    query {
+        publicaciones {
+            id
+            foto
+            descripcion
+            nombre
+            especie
+            raza
+            color
+            tamanho
+            sexo
+            ubicacionId
+        }
+    }";
+
+            var requestBody = new StringContent(
+                JsonConvert.SerializeObject(new { query = queryPublicaciones }),
+                Encoding.UTF8,
+                "application/json"
+            );
+
+            var response = await _httpClient.PostAsync("http://petfinder.somee.com/graphql/", requestBody);
+
+            if (!response.IsSuccessStatusCode)
+                return View("Error");
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var jsonResponse = JsonConvert.DeserializeObject<dynamic>(responseContent);
+
+            var publicaciones = jsonResponse.data.publicaciones.ToObject<List<Publicacion>>();
+
+            Publicacion publicacion = null;
+
+            // Reemplazamos la expresión lambda por un bucle foreach
+            foreach (var p in publicaciones)
+            {
+                if (p.Id == id)
+                {
+                    publicacion = p;
+                    break;
+                }
+            }
+
+            if (publicacion == null)
+                return NotFound();
+
+            return View(publicacion);
+        }
+
+
+
+
         public IActionResult AccountDetails()
         {
             return View();
@@ -29,13 +148,9 @@ namespace TuProyecto.Controllers
 
         public IActionResult EditAccount()
         {
-            return View(); 
+            return View();
         }
-        public ActionResult DetallesPerdida() => View("~/Views/Home/DetallesPerdida.cshtml");
-        
-            
-            
-        
+
 
         public IActionResult Cuidados() => View("~/Views/Home/Cuidados.cshtml");
 
---
+++ PetFinderMVC1/Models/Publicacion.cs
@@ -0,0 +1,17 @@
+﻿namespace PetFinderMVC1.Models
+{
+    public class Publicacion
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public string Foto { get; set; }
+        public string Descripcion { get; set; }
+        public string Raza { get; set; }
+        public string Especie { get; set; }
+        public string Color { get; set; }
+        public string Tamano { get; set; }
+        public string Sexo { get; set; }
+        public string UbicacionId { get; set; }
+    }
+
+}
